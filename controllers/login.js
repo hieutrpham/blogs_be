17d281262a601eb89943f3819f26c604ab153aad
Hieu <hieutrungpham91@gmail.com>
2025-01-01 08:15:19 +0200

    prettier

--- controllers/login.js
+++ controllers/login.js
@@ -1,34 +1,30 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
 
-loginRouter.post('/', async (request, response) => {
-    const {username, password} = request.body
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({ username });
+  const match = await bcrypt.compare(password, user.passwordHash);
+  if (!(user && match)) {
+    return response.status(401).json({ error: "invalid username or password" });
+  }
 
-    const user = await User.findOne({username})
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
 
-    // if user is found, decrypt the password hash
-    const passwordCorrect = user === null
-        ? false
-        : bcrypt.compare(password, user.passwordHash)
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  });
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({error: 'invalid username or password'})
-    }
+  response.status(200).send({
+    token,
+    username: user.username,
+    name: user.name,
+  });
+});
 
-    const userForToken = {
-        username: user.username,
-        id: user._id
-    }
-
-    const token = jwt.sign(userForToken, process.env.SECRET, {expiresIn: 60*60})
-
-    response.status(200).send({
-        token, 
-        username: user.username,
-        name: user.name
-    })
-})
-
-module.exports = loginRouter
+module.exports = loginRouter;
