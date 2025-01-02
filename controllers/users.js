c3b785dc37121c035911c75398aa2f178b3d5a62
Hieu <hieutrungpham91@gmail.com>
2025-01-02 13:57:50 +0200

    prettier

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,33 @@
-const userRouter = require('express').Router()
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
+const userRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-userRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('blog', {title: 1, author: 1, url: 1})
-    response.json(users)
-})
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blog", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
+  response.json(users);
+});
 
-userRouter.post('/', async (request, response) => {
-    const {username, name, password} = request.body
+userRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
 
-    if (password.length < 3) {
-        response.status(400).json({error: 'invalid username or password'})
-    }
+  if (password.length < 3) {
+    response.status(400).json({ error: "invalid username or password" });
+  }
 
-    const passwordHash = await bcrypt.hash(password, 10)
+  const passwordHash = await bcrypt.hash(password, 10);
 
-    const user = new User ({
-        username, name, passwordHash
-    })
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
 
-    const savedUser = await user.save()
-    response.status(201).json(savedUser)
-})
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
 
-module.exports = userRouter
+module.exports = userRouter;
